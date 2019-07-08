42d42e44f61c875a7df7cb3dc44605a42983df73
EddyVerbruggen <eddyverbruggen@gmail.com>
2019-07-08 09:34:58 +0200

    Download error: Error: Could not access local package. Error: The request timed out. #24

--- src/TNSRemotePackage.ts
+++ src/TNSRemotePackage.ts
@@ -16,29 +16,41 @@ export class TNSRemotePackage implements IRemotePackage {
   serverUrl: string;
 
   download(downloadSuccess: SuccessCallback<ILocalPackage>, downloadError?: ErrorCallback, downloadProgress?: SuccessCallback<DownloadProgress>): void {
-    getFile(this.downloadUrl).then(
-        (file: File) => {
-          let tnsLocalPackage: ILocalPackage = new TNSLocalPackage();
-          tnsLocalPackage.localPath = file.path;
-          tnsLocalPackage.deploymentKey = this.deploymentKey;
-          tnsLocalPackage.description = this.description;
-          tnsLocalPackage.label = this.label;
-          tnsLocalPackage.appVersion = this.appVersion;
-          tnsLocalPackage.isMandatory = this.isMandatory;
-          tnsLocalPackage.packageHash = this.packageHash;
-          tnsLocalPackage.isFirstRun = false;
-          // TODO (low prio) for failedInstall, see https://github.com/Microsoft/cordova-plugin-code-push/blob/055d9e625d47d56e707d9624c9a14a37736516bb/www/remotePackage.ts#L55 (but prolly not too relevant)
-          tnsLocalPackage.failedInstall = false;
-          tnsLocalPackage.serverUrl = this.serverUrl;
+    const onDownloadSuccess = (file: File) => {
+      let tnsLocalPackage: ILocalPackage = new TNSLocalPackage();
+      tnsLocalPackage.localPath = file.path;
+      tnsLocalPackage.deploymentKey = this.deploymentKey;
+      tnsLocalPackage.description = this.description;
+      tnsLocalPackage.label = this.label;
+      tnsLocalPackage.appVersion = this.appVersion;
+      tnsLocalPackage.isMandatory = this.isMandatory;
+      tnsLocalPackage.packageHash = this.packageHash;
+      tnsLocalPackage.isFirstRun = false;
+      // TODO (low prio) for failedInstall, see https://github.com/Microsoft/cordova-plugin-code-push/blob/055d9e625d47d56e707d9624c9a14a37736516bb/www/remotePackage.ts#L55 (but prolly not too relevant)
+      tnsLocalPackage.failedInstall = false;
+      tnsLocalPackage.serverUrl = this.serverUrl;
 
-          downloadSuccess(tnsLocalPackage);
+      downloadSuccess(tnsLocalPackage);
 
-          new TNSAcquisitionManager(this.deploymentKey, this.serverUrl).reportStatusDownload(tnsLocalPackage);
-        },
-        (e: any) => {
-          downloadError(new Error("Could not access local package. " + e));
-        }
-    );
+      new TNSAcquisitionManager(this.deploymentKey, this.serverUrl).reportStatusDownload(tnsLocalPackage);
+    };
+
+    // download, with a silly but effective retry mechanism
+    getFile(this.downloadUrl)
+        .then(onDownloadSuccess)
+        .catch(() => {
+          setTimeout(() => {
+            getFile(this.downloadUrl)
+                .then(onDownloadSuccess)
+                .catch(() => {
+                  setTimeout(() => {
+                    getFile(this.downloadUrl)
+                        .then(onDownloadSuccess)
+                        .catch((e: any) => downloadError(new Error("Could not download remote package. " + e)));
+                  }, 3000);
+                });
+          }, 3000);
+        });
   }
 
   abortDownload(abortSuccess?: SuccessCallback<void>, abortError?: ErrorCallback): void {
